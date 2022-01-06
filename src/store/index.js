79a1276a9bb4d54ddde9a7bfe100ab0da91edeb1
Vadym Ivaniuk <vadym_ivaniuk@epam.com>
2022-01-06 17:53:53 +0200

    removed redux devtools from createStore

--- src/store/index.js
+++ src/store/index.js
@@ -19,8 +19,7 @@ const store = createStore(
 			thunk,
 			clearFiltersWhenChangeMovieType,
 			updatePageWhenOnchangeFilters
-		),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+		)
 	)
 );
 
